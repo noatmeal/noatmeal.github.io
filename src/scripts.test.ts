f9c6c0da21f30ea82eed718a9a14acc550cd88ab
Noatmeal <noatmeal94@gmail.com>
2024-11-19 08:39:27 -0600

    Fiddling with names.

--- src/scripts.test.ts
+++ src/scripts.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { hello_world, HELLO_WORLD } from './index.ts'
+import { hello_world, HELLO_WORLD } from './scripts.ts'
 
 test('Should return hello_world', () => {
   expect(hello_world()).toBe(HELLO_WORLD);
