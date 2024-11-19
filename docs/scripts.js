d0a1fc9385092a590770074bab11b90843577916
Noatmeal <noatmeal94@gmail.com>
2024-11-19 17:30:31 -0600

    Diving in

--- docs/scripts.js
+++
@@ -1,7 +0,0 @@
-export const TITLE = "Some of My Notes About Math";
-export const placeTitle = () => {
-    const titleElements = [...document.getElementsByClassName("title")];
-    titleElements === null || titleElements === void 0 ? void 0 : titleElements.map((titleElement) => {
-        titleElement.append(TITLE);
-    });
-};
