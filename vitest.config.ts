16bb774c015e6d0cb00382d1ed0fb29b49733aea
Noatmeal <noatmeal94@gmail.com>
2024-11-19 08:35:50 -0600

    Adding coverage requirements and moving source files to a src folder.

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
+})
