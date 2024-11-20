3135c8c4bd66f33da7dcaf0ef362a31d03ed49ca
Noatmeal <noatmeal94@gmail.com>
2024-11-20 07:41:46 -0600

    Recursion theorem scaffolding.

--- docs/scripts.ts
+++
@@ -1,8 +0,0 @@
-export const TITLE = "Some of My Notes About Math";
-
-export const placeTitle = () => {
-  const titleElements = [...document.getElementsByClassName("title")];
-  titleElements?.map((titleElement) => {
-    titleElement.append(TITLE);
-  });
-};
